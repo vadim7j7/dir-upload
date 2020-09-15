22777b95f562e5a5c456fddacf09cba9f66530a2
Vadim Karnopelev <vadim7j7@gmail.com>
2020-09-15 13:43:56 +0800

    Configuration React for react-rails.

---
+++ app/javascript/components/FileManager/FileManager.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const FileManager = () => {
+  return (
+    <div>!!!</div>
+  );
+};
+
+export default FileManager;
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,14 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+// Support component names relative to this directory:
+const componentRequireContext = require.context("components", true);
+const ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
