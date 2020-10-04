6166730f4c8db5dd5dadb39425164ef209998ac0
Vadim Karnopelev <vadim7j7@gmail.com>
2020-10-04 14:20:34 +0800

    Add support: Digging all directories and files to add into the Archive

--- app/jobs/archive_job.rb
+++ app/jobs/archive_job.rb
@@ -1,11 +1,13 @@
 class ArchiveJob < ApplicationJob
+  require 'zip'
+
   """
   :EXAMPLE:
   [
     241,
     236,
     {
-      dir: 159,
+      dir: 154,
       items: []
     },
     {
@@ -26,8 +28,9 @@ class ArchiveJob < ApplicationJob
     Zip::OutputStream.open(temp_file) do |zip|
       build_source_map(zip, data)
     end
-
     temp_file.close
+
+    FileUtils.copy_file(temp_file.path, Rails.root.join('1.zip'))
   end
 
   private
@@ -40,7 +43,11 @@ class ArchiveJob < ApplicationJob
       if record.kind_of?(Hash)
         directory = Directory.find_by_id(record[:dir])
         path = [prev_dir, directory.name].compact.join('/')
-        build_source_map(zip, record[:items], path)
+        if record[:items].blank? # Add to the archive all directories and files from the directory.
+          build_all_sub(zip, directory, path)
+        else
+          build_source_map(zip, record[:items], path)
+        end
       else
         attach = ActiveStorage::Attachment.find_by_id(record)
         blob = attach.blob
@@ -50,7 +57,22 @@ class ArchiveJob < ApplicationJob
   end
 
   # @param[Zip::OutputStream] zip
-  # @param[ActiveStorage::Blob]
+  # @param[Directory] directory
+  # @param[String NilClass] path
+  def build_all_sub(zip, directory, path) # Digging all directories and files to add into the Archive
+    directory.children.each do |sub_directory|
+      path = [path, sub_directory.name].compact.join('/')
+      sub_directory.files.each do |file|
+        add_to_archive(zip, file.blob, path)
+      end
+
+      build_all_sub(zip, sub_directory,  path)
+    end
+  end
+
+  # Download and add the file into the Archive
+  # @param[Zip::OutputStream] zip
+  # @param[ActiveStorage::Blob] blob
   # @param[String NilClass] path
   def add_to_archive(zip, blob, path)
     full_path_name = [path, blob.filename.to_s].compact.join('/')
