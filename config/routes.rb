52934cb6d70f4b486d6e62a9c897264ba7bc5950
Vadim Karnopelev <vadim7j7@gmail.com>
2020-11-11 23:33:43 +0800

    Configure s3 encrypt Remove disc uploading

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   # Override direct uploads
   post '/rails/active_storage/direct_uploads', to: 'direct_uploads#create', as: :rails_direct_uploads_dirs
-  put '/rails/active_storage/disk/:encoded_token', to: 'disk#update', as: :update_rails_disk_service_dirs
 
   namespace :api, defaults: { format: :json } do
     resources :directories, only: :index do
